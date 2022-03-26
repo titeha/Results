8cf3e0f20a691fd3ac17bc1bea33605844467e02
Артемий Соболев <titeha@yandex.ru>
2022-03-27 02:22:55 +0700

    Доделаны тесты

--- Tests/TestsResultType/SuccessedResultTests.cs
+++ Tests/TestsResultType/SuccessedResultTests.cs
@@ -1,12 +1,8 @@
-﻿using FluentAssertions;
+﻿using System;
+
+using FluentAssertions;
 
 using ResultType;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xunit;
 
---
+++ Tests/TestsResultType/TryTestBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestsResultType
+{
+ public abstract class TryTestBase : TestBase
+ {
+  protected TryTestBase()
+  {
+   FuncExecuted = false;
+  }
+
+  protected static readonly Exception Exception = new Exception(ErrorMessage);
+  protected const string ErrorHandlerMessage = "Error message from error handler";
+  protected static readonly Func<Exception, string> ErrorHandler = exc => ErrorHandlerMessage;
+  protected static readonly Func<Exception, E> ErrorHandlerE = exc => E.Value;
+
+  protected bool FuncExecuted;
+
+  protected void Action() => FuncExecuted = true;
+  protected void Action_T(T _) => FuncExecuted = true;
+
+  protected T Throwing_Func_T() => throw Exception;
+  protected K Throwing_Func_T_K(T _) => throw Exception;
+  protected void Throwing_Action() => throw Exception;
+  protected void Throwing_Action_T(T _) => throw Exception;
+  protected T Func_T()
+  {
+   FuncExecuted = true;
+   return T.Value;
+  }
+
+  protected Task Func_Task()
+  {
+   FuncExecuted = true;
+   return Task.CompletedTask;
+  }
+  protected Task Throwing_Func_Task() => Exception.AsTask();
+
+  protected Task<T> Func_Task_T()
+  {
+   FuncExecuted = true;
+   return T.Value.AsTask();
+  }
+
+  protected Task<T> Throwing_Func_Task_T() => Exception.AsTask<T>();
+ }
+}
