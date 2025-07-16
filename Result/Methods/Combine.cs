91a65ec4302a22ae48d9a5a151cd08780eb65e79
Артемий Соболев <titeha@yandex.ru>
2025-07-16 15:06:24 +0700

    Добавил базовую реализация объединения результатов.

---
+++ Result/Methods/Combine.cs
@@ -0,0 +1,17 @@
+﻿namespace ResultType;
+
+public partial struct Result
+{
+  public static Result Combine(IEnumerable<Result> results, string errorMessageSeparator = "\n")
+  {
+    List<string> failedResult = [];
+
+    foreach (var result in results.ToList())
+      if (result.IsFailure)
+        failedResult.Add(result.Error!);
+
+    return failedResult.Count == 0
+      ? Success()
+      : Failure(string.Join(errorMessageSeparator, failedResult));
+  }
+}
