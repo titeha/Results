3358c19923edd1b1c12ce3e02b59fbb67a958f11
Artemy Sobolev <titeha@yandex.ru>
2026-03-24 09:22:55 +0700

    Добавление try для UnitResult + тесты

---
+++ Result/Methods/Try.UnitResult.cs
@@ -0,0 +1,38 @@
+﻿namespace ResultType;
+
+public partial struct Result
+{
+  public static UnitResult<E?> Try<E>(Action action, Func<Exception, E> errorHandler)
+  {
+    ArgumentNullException.ThrowIfNull(action);
+    ArgumentNullException.ThrowIfNull(errorHandler);
+
+    try
+    {
+      action();
+      return UnitResult.Success<E>();
+    }
+    catch (Exception exception)
+    {
+      E error = errorHandler(exception);
+      return UnitResult.Failure<E?>(error);
+    }
+  }
+
+  public static async Task<UnitResult<E?>> Try<E>(Func<Task> action, Func<Exception, E> errorHandler)
+  {
+    ArgumentNullException.ThrowIfNull(action);
+    ArgumentNullException.ThrowIfNull(errorHandler);
+
+    try
+    {
+      await action().DefaultAwait();
+      return UnitResult.Success<E>();
+    }
+    catch (Exception exception)
+    {
+      E error = errorHandler(exception);
+      return UnitResult.Failure<E?>(error);
+    }
+  }
+}
---
+++ Tests/TestsResultType/TryUnitResult.Tests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using ResultType;
+
+using Xunit;
+
+namespace TestsResultType;
+
+public class TryUnitResultTests : TryTestBase
+{
+  [Fact]
+  public void ResultTry_E_execute_action_success_result_expected()
+  {
+    UnitResult<E?> result = Result.Try(Action, ErrorHandlerE);
+
+    result.IsSuccess.Should().BeTrue();
+    result.IsFailure.Should().BeFalse();
+    FuncExecuted.Should().BeTrue();
+  }
+
+  [Fact]
+  public void ResultTry_E_execute_action_failed_failed_result_expected()
+  {
+    UnitResult<E?> result = Result.Try(Throwing_Action, ErrorHandlerE);
+
+    result.IsFailure.Should().BeTrue();
+    result.Error.Should().Be(E.Value);
+  }
+
+  [Fact]
+  public async Task ResultTry_Async_E_execute_action_success_result_expected()
+  {
+    UnitResult<E?> result = await Result.Try(Func_Task, ErrorHandlerE);
+
+    result.IsSuccess.Should().BeTrue();
+    result.IsFailure.Should().BeFalse();
+    FuncExecuted.Should().BeTrue();
+  }
+
+  [Fact]
+  public async Task ResultTry_Async_E_execute_action_failed_failed_result_expected()
+  {
+    UnitResult<E?> result = await Result.Try(Throwing_Func_Task, ErrorHandlerE);
+
+    result.IsFailure.Should().BeTrue();
+    result.Error.Should().Be(E.Value);
+  }
+}
